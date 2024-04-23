8e7ef53b10e52fc7b8b8c7393efc93f10efc40cc
ngayMaivanden <datlai304@gmail.com>
2024-04-23 15:28:24 +0700

    code build deloy

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = require("dotenv");
 const compression_1 = __importDefault(require("compression"));
 const helmet_1 = __importDefault(require("helmet"));
 const cors_1 = __importDefault(require("cors"));
@@ -13,6 +14,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const mongo_connect_1 = __importDefault(require("./db/mongo.connect"));
 const errorHandler_1 = __importDefault(require("./helpers/errorHandler"));
 const body_parser_1 = __importDefault(require("body-parser"));
+(0, dotenv_1.config)();
 const app = (0, express_1.default)();
 mongo_connect_1.default.Connect();
 app.use((0, helmet_1.default)());
@@ -23,7 +25,7 @@ app.use(body_parser_1.default.json());
 // for parsing application/x-www-form-urlencoded
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000', // Cho phép truy cập từ origin này
+    origin: process.env.CLIENT_URL, // Cho phép truy cập từ origin này
     methods: ['GET', 'POST'], // Chỉ cho phép các phương thức GET và POST
     allowedHeaders: ['Content-Type', 'Authorization'], // Chỉ
     credentials: true
@@ -32,6 +34,6 @@ app.use('', routers_1.default);
 app.use((error, req, res, next) => {
     return (0, errorHandler_1.default)(error, req, res, next);
 });
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log('comming');
 });
