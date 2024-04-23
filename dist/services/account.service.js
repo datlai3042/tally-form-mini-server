0aa7c09dc52bdb79a8a80acf56591bd3ceec7f24
ngayMaivanden <datlai304@gmail.com>
2024-04-23 20:44:18 +0700

    test them cookies

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const dataResponse_utils_1 = require("../utils/dataResponse.utils");
 class AccountService {
     static async me(req, res, next) {
         console.log('api request');
         const { user } = req;
         // console.log({ user })
+        const expireToken = (0, dataResponse_utils_1.setCookieResponse)(res, dataResponse_utils_1.expriresAT, 'test', 'success', { httpOnly: true });
         return { user };
     }
 }
--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Response } from 'express'
 import { CustomRequest } from '~/type'
+import { expriresAT, setCookieResponse } from '~/utils/dataResponse.utils'
 
 class AccountService {
       static async me(req: CustomRequest, res: Response, next: NextFunction) {
             console.log('api request')
             const { user } = req
             // console.log({ user })
+            const expireToken = setCookieResponse(res, expriresAT, 'test', 'success', { httpOnly: true })
+
             return { user }
       }
 }
